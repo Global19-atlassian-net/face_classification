4ae28de82694cf419880aa8819a4e43de1aa11b3
Wilder Rodrigues <wilder.rodrigues@irdeto.com>
2017-07-05 12:17:35 +0200

    Add method to process image in the resource.

--- src/web/faces.py
+++ src/web/faces.py
@@ -1,12 +1,26 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response, request, abort, redirect
+import logging
+
+import image_processor as ip
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Welcome to Ekholabs' Face Classifier."
+    return redirect("https://ekholabs.ai", code=302)
 
-from flask import make_response
+@app.route('/process', methods=['POST'])
+def upload():
+    try:
+        image = request.files.get('face', '')
+        return ip.process_image(image)
+    except Exception as err:
+        logging.error('An error has occurred whilst processing the file: "{0}"'.format(err))
+        abort(400)
+
+@app.errorhandler(400)
+def bad_request(erro):
+    return make_response(jsonify({'error': 'We cannot process the file sent in the request.'}), 400)
 
 @app.errorhandler(404)
 def not_found(error):
